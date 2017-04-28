9881fbc43f3fd0e3b60391b6b5b746e3c51ed93b
noldors <romanov@noldor.pro>
2017-04-29 01:34:57 +0300

    Translation for emails

---
+++ app/Notifications/ResetPasswordNotification.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class ResetPasswordNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * The password reset token.
+     *
+     * @var string
+     */
+    public $token;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject('Сброс пароля')
+            ->greeting('Привет от куратора из ФСБ!')
+            ->salutation('Помни, куратор любит тебя!')
+            ->line('Вы получили данное письмо, так как инифиировали процесс сброса пароля для вашего аккаунта.')
+            ->action('Сбросить пароль', route('password.reset', $this->token))
+            ->line('Если вы не запрашивали сброс пароля, сообщите о данном происшествии вашему куратору из ФСБ.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
